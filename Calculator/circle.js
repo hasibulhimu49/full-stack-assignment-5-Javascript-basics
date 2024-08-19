20282736198b2b4d21ccefbcdd66fdd5bccad080
hasibulhimu49 <mohammadhasibulhasanpga4149@gmail.com>
2024-08-19 13:46:55 +0600

    Basic calculator assignment

---
+++ Calculator/circle.js
@@ -0,0 +1,5 @@
+
+// (area of circle = 3.1416 * radius * radius)
+const radius = Number(prompt("Please enter a circle radius : "));
+const circleArea = 3.1416 * radius * radius;
+document.getElementById("circleArea").innerHTML = (`Circle Area = ${circleArea}`);
